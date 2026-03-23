176e1fe77bfccc643fca858eac7cb00d1c3f8b21
Vladyslav <vlad.mezhevyi@gmail.com>
2026-03-23 20:01:29 +0000

    feat(FE): create basic search store

---
+++ apps/forge/src/app/features/search/store/search.store.ts
@@ -0,0 +1,102 @@
+import { computed, inject } from '@angular/core';
+import {
+  GetRepositoryResponse,
+  GetUserResponse,
+  SearchRepositoriesResponse,
+  SearchType,
+  SearchUsersResponse,
+} from '@drc/shared/contracts';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { SearchApi } from '../../../core/api/search/search.api';
+import { catchError, EMPTY, pipe, switchMap, tap } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+
+export interface SearchState {
+  query: string;
+  activeType: SearchType;
+  repositories: SearchRepositoriesResponse['items'];
+  users: SearchUsersResponse['items'];
+  isLoading: boolean;
+  error: string | null;
+}
+
+const initialState: SearchState = {
+  query: '',
+  activeType: 'repositories',
+  repositories: [],
+  users: [],
+  isLoading: false,
+  error: null,
+};
+
+export const SearchStore = signalStore(
+  withState(initialState),
+  withComputed(({ activeType, repositories, users, isLoading, error }) => ({
+    isEmpty: computed(() => {
+      const array = activeType() === 'repositories' ? repositories() : users();
+      return !isLoading() && !array.length;
+    }),
+    hasError: computed(() => error() !== null),
+  })),
+  withMethods((store, api = inject(SearchApi)) => ({
+    searchRepositories: rxMethod<string>(
+      pipe(
+        tap((query) =>
+          patchState(store, {
+            query,
+            activeType: 'repositories',
+            isLoading: true,
+            error: null,
+          }),
+        ),
+        switchMap((query) =>
+          api.searchRepositories({ q: query }).pipe(
+            tap((response) =>
+              patchState(store, {
+                repositories: response.items || [],
+                isLoading: false,
+              }),
+            ),
+            catchError((error: HttpErrorResponse) => {
+              patchState(store, { isLoading: false, error: error.message });
+              return EMPTY;
+            }),
+          ),
+        ),
+      ),
+    ),
+    searchUsers: rxMethod<string>(
+      pipe(
+        tap((query) =>
+          patchState(store, {
+            query,
+            activeType: 'users',
+            isLoading: true,
+            error: null,
+          }),
+        ),
+        switchMap((query) =>
+          api.searchUsers({ q: query }).pipe(
+            tap((response) =>
+              patchState(store, {
+                users: response.items || [],
+                isLoading: false,
+              }),
+            ),
+            catchError((error: HttpErrorResponse) => {
+              patchState(store, { isLoading: false, error: error.message });
+              return EMPTY;
+            }),
+          ),
+        ),
+      ),
+    ),
+  })),
+);
